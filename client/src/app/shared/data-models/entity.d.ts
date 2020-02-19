bf306a857abf49a9f02770ee2b2f5e8eb64aef9c
Victor Espinosa Mateu <vicesmat@gmail.com>
2020-02-19 13:22:22 +0100

    added entities

---
+++ client/src/app/shared/data-models/entity.d.ts
@@ -0,0 +1,10 @@
+export interface Entity {
+    id: string;
+    type: string;
+    source: string;
+    dataProvider: string;
+    description: string;
+    location: {
+        coordinates: number[];
+    };
+}
