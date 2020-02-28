4cf91ef5215a167180f34919b3e910f37e71a5b9
Victor Espinosa Mateu <vicesmat@gmail.com>
2020-02-28 09:35:02 +0100

    added request schema

---
+++ myApp/routes/airQualityObserved.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+const request = require('request');
+
+const params = {
+  type: 'EEA_POLLUTION',
+  options: 'keyValues',
+  limit: '500'
+};
+
+const heads = {
+  'fiware-service': 'openiot',
+  'fiware-servicepath': '/EEA_POLLUTION',
+};
+
+router.get('/', function (req, res, next) {
+
+  request({ url: 'http://localhost:1026/v2/entities', qs: params, headers: heads, json: true }, (e, r, b) => {
+    if (e) { return console.log(e); }
+    res.send(b);
+  });
+
+});
+
+module.exports = router;
