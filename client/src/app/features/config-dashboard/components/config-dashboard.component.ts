d230f8ed92ccdd43206f11e712e32fe36228e9a9
Victor Espinosa Mateu <victor.espinosa.mateu@everis.com>
2020-03-11 13:19:49 +0100

    added forms

--- client/src/app/features/config-dashboard/components/config-dashboard.component.ts
+++ client/src/app/features/config-dashboard/components/config-dashboard.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-config-dashboard',
   templateUrl: './config-dashboard.component.html',
   styleUrls: ['./config-dashboard.component.scss'],
 })
-export class ConfigDashboardComponent {
+export class ConfigDashboardComponent implements OnInit {
+
+  protected contextBrokers: any[] = [];
+  private defaultContextName: string = 'New Context Broker';
+
+  public ngOnInit(): void {
+  }
+
+  protected onAddContextBroker(): void {
+    this.contextBrokers.push({
+      header: this.defaultContextName,
+      name: this.defaultContextName,
+      form: new FormGroup({
+        name: new FormControl(this.defaultContextName),
+        url: new FormControl(),
+        port: new FormControl(),
+      }),
+    });
+  }
+
+  protected onGeneralConfigChange(index: number): void {
+    const name: string = this.contextBrokers[index].form.value.name;
+    const url: string = this.contextBrokers[index].form.value.url;
+    const port: string = this.contextBrokers[index].form.value.port;
+    this.contextBrokers[index].header = name + (name && url ? ' - ' + url : '') + (name && url && port ? ':' + port : '');
+  }
+
 }
--- client/src/app/features/config-dashboard/config-dashboard.module.ts
+++ client/src/app/features/config-dashboard/config-dashboard.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfigDashboardComponent } from './components/config-dashboard.component';
 import { ConfigDashboardRoutingModule } from './config-dashboard-routing.module';
+import { AccordionModule } from 'primeng/accordion';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,6 +15,11 @@ import { ConfigDashboardRoutingModule } from './config-dashboard-routing.module'
   imports: [
     CommonModule,
     ConfigDashboardRoutingModule,
+    AccordionModule,
+    CardModule,
+    ButtonModule,
+    InputTextModule,
+    ReactiveFormsModule,
   ],
 })
 export class ConfigDashboardModule { }
