ffe796254b0be2d1e916e7100854524f03b135b0
Victor Espinosa Mateu <victor.espinosa.mateu@everis.com>
2020-03-31 18:43:51 +0200

    fixed cygnus and comet connection checking bug

--- server/routes/checkHealth.js
+++ server/routes/checkHealth.js
@@ -3,11 +3,31 @@ var router = express.Router();
 const request = require('request');
 const utils = require('./utils');
 
-router.get('/', function (req, res, next) {
+router.get('/broker', function (req, res, next) {
   const url = utils.parseUrl(req.query.url) + '/version';
 
   request({ url: url, json: true }, (e, r, b) => {
-    if (b && b.orion) res.send(true);
+    if (b && b.orion && b.orion.version) res.send(true);
+    else res.send(false);
+  });
+
+});
+
+router.get('/cygnus', function (req, res, next) {
+  const url = utils.parseUrl(req.query.url) + '/v1/version';
+
+  request({ url: url, json: true }, (e, r, b) => {
+    if (b && b.version) res.send(true);
+    else res.send(false);
+  });
+
+});
+
+router.get('/comet', function (req, res, next) {
+  const url = utils.parseUrl(req.query.url) + '/version';
+
+  request({ url: url, json: true }, (e, r, b) => {
+    if (b && b.version) res.send(true);
     else res.send(false);
   });
 
