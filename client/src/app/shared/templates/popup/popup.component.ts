34e89f704c99200b947126512b53bf4d49f59c80
Victor Espinosa Mateu <victor.espinosa.mateu@everis.com>
2020-04-02 18:12:19 +0200

    fixed long strings

--- client/src/app/shared/templates/popup/popup.component.ts
+++ client/src/app/shared/templates/popup/popup.component.ts
@@ -11,6 +11,7 @@ export class PopupComponent implements OnInit {
     @Input() public entity: any;
 
     protected attrs: any;
+    private maxNumberChars: number = 45;
 
     public ngOnInit(): void {
         this.attrs = Object.entries(this.entity).filter(a => typeof a[1] !== 'object').map(a => [a[0], this.transformAttr(a[1])]);
@@ -20,6 +21,9 @@ export class PopupComponent implements OnInit {
         const dateExp: RegExp = new RegExp(/.*-.*-.*:.*:.*\..*Z/);
         if (dateExp.test(attr)) {
             return moment(attr).format('DD/MM/YYYY HH:mm:ss');
+        }
+        if (typeof attr === 'string' && attr.length > this.maxNumberChars) {
+            return attr.substring(0, this.maxNumberChars) + '...';
         }
         return attr;
     }
