145eb28131fb8f9a266734160583d89abf99f580
Victor Espinosa Mateu <victor.espinosa.mateu@everis.com>
2020-04-02 12:15:14 +0200

    added mapping to icons

--- client/src/app/features/map-dashboard/services/layer-service/layer-service.ts
+++ client/src/app/features/map-dashboard/services/layer-service/layer-service.ts
@@ -28,14 +28,71 @@ export class LayerService {
 
     public getParentKey(type: string): string {
         switch (type) {
-            case 'WaterQualityObserved':
+            case 'Alert':
+                return 'alerts';
+
+            case 'Park':
+            case 'Garden':
+            case 'FlowerBed':
+            case 'FlowerBed':
+                return 'smartEnvironment';
+
+            case 'AeroAllergenObserved':
             case 'AirQualityObserved':
+            case 'WaterQualityObserved':
+            case 'NoiseLevelObserved':
                 return 'environment';
-            case 'OffStreetParking':
+
+            case 'PointOfInterest':
+            case 'Beach':
+            case 'Museum':
+            case 'TouristInformationCenter':
+                return 'pointOfInterest';
+
+            case 'Open311:ServiceType':
+            case 'Open311:ServiceRequest':
+                return 'civicIssuesTracking';
+
+            case 'Streetlight':
+            case 'StreetlightGroup':
+            case 'StreetlightModel':
+            case 'StreetlightControlCabinet':
+                return 'streetLighting';
+
+            case 'Device':
+            case 'DeviceModel':
+                return 'device';
+
+            case 'TrafficFlowObserved':
+            case 'CrowdFlowObserved':
+            case 'BikeHireDockingStation':
+            case 'EVChargingStation':
+            case 'Road':
+            case 'RoadSegment':
+            case 'Vehicle':
+            case 'VehicleModel':
                 return 'transport';
+
+            case 'KeyPerformanceIndicator':
+                return 'indicators';
+
+            case 'WasteContainerIsle':
+            case 'WasteContainerModel':
+            case 'WasteContainer':
+                return 'wasteManagement';
+
+            case 'OffStreetParking':
+            case 'ParkingAccess':
+            case 'OnStreetParking':
+            case 'ParkingGroup':
+            case 'ParkingSpot':
+                return 'parking';
+
+            case 'WeatherForecast':
+                return 'weather';
+
             default:
                 return 'generic';
-
         }
     }
 
--- client/src/app/shared/misc/leaflet-icons.ts
+++ client/src/app/shared/misc/leaflet-icons.ts
@@ -6,8 +6,74 @@ export class LeafletIcons {
 
     public static readonly icons: { [key: string]: L.Icon } = {
 
+        alerts: L.icon({
+            iconUrl: '../../assets/icons/Alerts.png',
+            iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
+            iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+        }),
+
+        smartEnvironment: L.icon({
+            iconUrl: '../../assets/icons/SmartEnvironment.png',
+            iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
+            iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+        }),
+
         environment: L.icon({
             iconUrl: '../../assets/icons/Environment.png',
+            iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
+            iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+        }),
+
+        pointOfInterest: L.icon({
+            iconUrl: '../../assets/icons/PointOfInterest.png',
+            iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
+            iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+        }),
+
+        civicIssuesTracking: L.icon({
+            iconUrl: '../../assets/icons/CivicIssueTracking.png',
+            iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
+            iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+        }),
+
+        streetLighting: L.icon({
+            iconUrl: '../../assets/icons/StreetLightning.png',
+            iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
+            iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+        }),
+
+        device: L.icon({
+            iconUrl: '../../assets/icons/Devices.png',
+            iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
+            iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+        }),
+
+        transport: L.icon({
+            iconUrl: '../../assets/icons/Generic.png',
+            iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
+            iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+        }),
+
+        indicators: L.icon({
+            iconUrl: '../../assets/icons/Transportation.png',
+            iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
+            iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+        }),
+
+        wasteManagement: L.icon({
+            iconUrl: '../../assets/icons/WasteManagement.png',
+            iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
+            iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+        }),
+
+        parking: L.icon({
+            iconUrl: '../../assets/icons/Parking.png',
+            iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
+            iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+        }),
+
+        weather: L.icon({
+            iconUrl: '../../assets/icons/Weather.png',
             iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
             iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
         }),
