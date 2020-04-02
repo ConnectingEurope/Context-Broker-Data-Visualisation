a32ad28684b1666455453ec0c88e4325671917b8
Victor Espinosa Mateu <victor.espinosa.mateu@everis.com>
2020-04-02 17:10:51 +0200

    improved position of popup

--- client/src/app/shared/misc/leaflet-icons.ts
+++ client/src/app/shared/misc/leaflet-icons.ts
@@ -10,78 +10,91 @@ export class LeafletIcons {
             iconUrl: '../../assets/icons/Alerts.png',
             iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
             iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+            popupAnchor: [0, -LeafletIcons.iconSize],
         }),
 
         smartEnvironment: L.icon({
             iconUrl: '../../assets/icons/SmartEnvironment.png',
             iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
             iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+            popupAnchor: [0, -LeafletIcons.iconSize],
         }),
 
         environment: L.icon({
             iconUrl: '../../assets/icons/Environment.png',
             iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
             iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+            popupAnchor: [0, -LeafletIcons.iconSize],
         }),
 
         pointOfInterest: L.icon({
             iconUrl: '../../assets/icons/PointOfInterest.png',
             iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
             iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+            popupAnchor: [0, -LeafletIcons.iconSize],
         }),
 
         civicIssuesTracking: L.icon({
             iconUrl: '../../assets/icons/CivicIssueTracking.png',
             iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
             iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+            popupAnchor: [0, -LeafletIcons.iconSize],
         }),
 
         streetLighting: L.icon({
             iconUrl: '../../assets/icons/StreetLightning.png',
             iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
             iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+            popupAnchor: [0, -LeafletIcons.iconSize],
         }),
 
         device: L.icon({
             iconUrl: '../../assets/icons/Devices.png',
             iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
             iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+            popupAnchor: [0, -LeafletIcons.iconSize],
         }),
 
         transport: L.icon({
             iconUrl: '../../assets/icons/Generic.png',
             iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
             iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+            popupAnchor: [0, -LeafletIcons.iconSize],
         }),
 
         indicators: L.icon({
             iconUrl: '../../assets/icons/Transportation.png',
             iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
             iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+            popupAnchor: [0, -LeafletIcons.iconSize],
         }),
 
         wasteManagement: L.icon({
             iconUrl: '../../assets/icons/WasteManagement.png',
             iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
             iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+            popupAnchor: [0, -LeafletIcons.iconSize],
         }),
 
         parking: L.icon({
             iconUrl: '../../assets/icons/Parking.png',
             iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
             iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+            popupAnchor: [0, -LeafletIcons.iconSize],
         }),
 
         weather: L.icon({
             iconUrl: '../../assets/icons/Weather.png',
             iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
             iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+            popupAnchor: [0, -LeafletIcons.iconSize],
         }),
 
         generic: L.icon({
             iconUrl: '../../assets/icons/Generic.png',
             iconSize: [LeafletIcons.iconSize, LeafletIcons.iconSize],
             iconAnchor: [LeafletIcons.iconSize / 2, LeafletIcons.iconSize],
+            popupAnchor: [0, -LeafletIcons.iconSize],
         }),
 
     };
